49a77c49122e98778d7fc01d93d9ffa7afca6891
PoliJun <rivenloving@gmail.com>
2024-01-02 16:47:59 +0800

    created

---
+++ src/main/java/com/github/polijun/wavup/WavupApplication.java
@@ -0,0 +1,13 @@
+package com.github.polijun.wavup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavupApplication.class, args);
+	}
+
+}
