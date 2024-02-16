584b2ad8ae22095456dc553d528564d37662232f
PoliJun <rivenloving@gmail.com>
2024-02-17 00:30:04 +0800

    initial spring boot

---
+++ src/test/java/com/github/polijun/wavup/WavupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.polijun.wavup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
