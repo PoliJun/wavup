ad7c44251983b6abd4cdc375b9bf7b530fd96d3a
PoliJun <rivenloving@gmail.com>
2024-02-16 23:56:13 +0800

    created

--- src/main/java/com/github/polijun/wavup/exception/DuplicateUsernameException.java
+++
@@ -1,7 +0,0 @@
-package com.github.polijun.wavup.exception;
-
-public class DuplicateUsernameException extends RuntimeException{
-    public DuplicateUsernameException(String msg) {
-        super("Username already exists: " + msg);
-    }
-}
--- src/main/java/com/github/polijun/wavup/exception/UserAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package com.github.polijun.wavup.exception;
-
-public class UserAlreadyExists extends RuntimeException {
-    public UserAlreadyExists(String msg) {
-        super("Username already exists: " + msg);
-    }
-}
