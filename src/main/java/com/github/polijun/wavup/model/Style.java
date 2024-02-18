597a8de8369f7d3ba812c096df6aab278fd6ddab
PoliJun <rivenloving@gmail.com>
2024-02-19 01:26:59 +0800

    Refactor ProductController and Product model classes

--- src/main/java/com/github/polijun/wavup/model/Style.java
+++ src/main/java/com/github/polijun/wavup/model/Style.java
@@ -1,15 +1,9 @@
 package com.github.polijun.wavup.model;
 
-import java.util.List;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
