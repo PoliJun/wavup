d48ccdafc2126b889a7af513a07db8196a150aea
PoliJun <rivenloving@gmail.com>
2024-02-19 16:16:01 +0800

    Refactor ProductController and UserController

--- src/main/java/com/github/polijun/wavup/controller/ProductController.java
+++ src/main/java/com/github/polijun/wavup/controller/ProductController.java
@@ -7,6 +7,9 @@ import com.github.polijun.wavup.model.Style;
 import com.github.polijun.wavup.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,21 +24,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ProductController {
     private final ProductService productService;
 
-    @PostMapping("/bystyle")
-    public List<Product> getByStyles(@RequestBody List<Style> styles) {
-        List<Product> products = productService.getProductsByStyles(styles);
-        return products;
-    }
-
-    @PostMapping("/create")
-    public Product createProduct(@RequestBody Product product) {
-        return productService.createProduct(product);
-    }
-
     @GetMapping("/all")
-    public List<Product> getAllProducts() {
-        List<Product> products = productService.getAllProducts();
-        return products;
+    public ResponseEntity<List<Product>> getAllProducts() {
+        try {
+            return ResponseEntity.ok().body(productService.getAllProducts());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
+    
 }
--- src/main/java/com/github/polijun/wavup/security/user/UserController.java
+++ src/main/java/com/github/polijun/wavup/security/user/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -74,7 +73,7 @@ public class UserController {
         }
     }
 
-    @DeleteMapping("/{id}") // Added delete endpoint
+    @DeleteMapping("/{id}") 
     public ResponseEntity<String> deleteMethodName(@PathVariable String id) {
         try {
             userService.deleteUser(Long.parseLong(id));
