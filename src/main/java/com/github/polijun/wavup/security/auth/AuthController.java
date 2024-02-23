a0316d8665ee21a8df5e91cd9acacad1f20a1322
PoliJun <rivenloving@gmail.com>
2024-02-23 20:24:26 +0800

    Add AuthController for authentication and registration

---
+++ src/main/java/com/github/polijun/wavup/security/auth/AuthController.java
@@ -0,0 +1,29 @@
+package com.github.polijun.wavup.security.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok().body(authenticationService.authenticate(authenticationRequest));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> postMethodName(@RequestBody RegisterRequest authenticationRequest) {
+        return  ResponseEntity.ok().body(authenticationService.register(authenticationRequest));
+    }
+    
+}
